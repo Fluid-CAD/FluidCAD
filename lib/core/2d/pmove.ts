9b5c166383497b0fac28a0b5ec4525d240f97816
Marwan Aouida <marwan@aouida.com>
2026-03-08 10:46:55 +0100

    sketcher: add pmove for polar move

---
+++ lib/core/2d/pmove.ts
@@ -0,0 +1,19 @@
+import { PolarMove } from "../../features/2d/pmove.js";
+import { registerBuilder, SceneParserContext } from "../../index.js";
+
+interface PolarMoveFunction {
+  (radius: number, angle: number): PolarMove;
+}
+
+function build(context: SceneParserContext): PolarMoveFunction {
+  return function pmove() {
+    const radius = arguments[0] as number;
+    const angle = (arguments[1] as number) * Math.PI / 180;
+    const pmove = new PolarMove(radius, angle);
+    context.addSceneObject(pmove);
+
+    return pmove;
+  }
+}
+
+export default registerBuilder(build);

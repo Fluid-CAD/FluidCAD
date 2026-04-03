dbb01d04c611950192a4a9175311b639212b226f
Marwan Aouida <marwan@aouida.com>
2026-04-03 10:57:53 +0100

    feat: update exposed edges/faces on Loft and allow applying filters on them

--- lib/features/loft.ts
+++ lib/features/loft.ts
@@ -3,30 +3,40 @@ import { Explorer } from "../oc/explorer.js";
 import { LoftOps } from "../oc/loft-ops.js";
 import { Wire } from "../common/wire.js";
 import { Face } from "../common/face.js";
+import { Edge } from "../common/edge.js";
+import { Shape } from "../common/shape.js";
 import { Extrudable } from "../helpers/types.js";
 import { FaceMaker2 } from "../oc/face-maker2.js";
+import { LazySceneObject } from "./lazy-scene-object.js";
+import { FaceFilterBuilder } from "../filters/face/face-filter.js";
+import { EdgeFilterBuilder } from "../filters/edge/edge-filter.js";
+import { FilterBuilderBase } from "../filters/filter-builder-base.js";
+import { ShapeFilter } from "../filters/filter.js";
+import { FaceOps } from "../oc/face-ops.js";
+import { Plane } from "../math/plane.js";
+import { ILoft } from "../core/interfaces.js";
 
-export class Loft extends SceneObject {
-  private _faces: SceneObject[] = [];
+export class Loft extends SceneObject implements ILoft {
+  private _profiles: SceneObject[] = [];
 
   constructor(...profiles: SceneObject[]) {
     super();
-    this._faces = profiles;
+    this._profiles = profiles;
   }
 
-  get faces(): SceneObject[] {
-    return this._faces;
+  get profiles(): SceneObject[] {
+    return this._profiles;
   }
 
   build(context: BuildSceneObjectContext) {
-    if (this.faces.length < 2) {
+    if (this.profiles.length < 2) {
       throw new Error("Loft requires at least two profiles.");
     }
 
     const allWires: Wire[] = [];
 
-    for (const face of this.faces) {
-      const wires = this.getWiresFromSceneObject(face);
+    for (const profile of this.profiles) {
+      const wires = this.getWiresFromSceneObject(profile);
 
       if (wires.length === 0) {
         throw new Error("Could not extract wire from profile.");
@@ -39,11 +49,126 @@ export class Loft extends SceneObject {
 
     const newShapes = LoftOps.makeLoft(allWires);
 
-    for (const face of this.faces) {
-      face.removeShapes(this);
+    for (const profile of this.profiles) {
+      profile.removeShapes(this);
     }
 
     this.addShapes(newShapes);
+
+    // Classify faces into start/end/side using profile planes
+    const firstPlane = this.getProfilePlane(this.profiles[0]);
+    const lastPlane = this.getProfilePlane(this.profiles[this.profiles.length - 1]);
+
+    const startFaces: Face[] = [];
+    const endFaces: Face[] = [];
+    const sideFaces: Face[] = [];
+
+    for (const shape of newShapes) {
+      const faces = Explorer.findFacesWrapped(shape);
+      for (const f of faces) {
+        if (firstPlane && FaceOps.faceOnPlaneWrapped(f as Face, firstPlane)) {
+          startFaces.push(f as Face);
+        } else if (lastPlane && FaceOps.faceOnPlaneWrapped(f as Face, lastPlane)) {
+          endFaces.push(f as Face);
+        } else {
+          sideFaces.push(f as Face);
+        }
+      }
+    }
+
+    this.setState('start-faces', startFaces);
+    this.setState('end-faces', endFaces);
+    this.setState('side-faces', sideFaces);
+  }
+
+  private getProfilePlane(profile: SceneObject): Plane | null {
+    if ('getPlane' in profile && typeof (profile as any).getPlane === 'function') {
+      return (profile as Extrudable).getPlane();
+    }
+    return null;
+  }
+
+  startFaces(...args: (number | FaceFilterBuilder)[]): SceneObject {
+    const suffix = this.buildSuffix('start-faces', args);
+    return new LazySceneObject(`${this.generateUniqueName(suffix)}`,
+      () => {
+        const faces = this.getState('start-faces') as Face[] || [];
+        return this.resolveShapes(faces, args);
+      });
+  }
+
+  endFaces(...args: (number | FaceFilterBuilder)[]): SceneObject {
+    const suffix = this.buildSuffix('end-faces', args);
+    return new LazySceneObject(`${this.generateUniqueName(suffix)}`,
+      () => {
+        const faces = this.getState('end-faces') as Face[] || [];
+        return this.resolveShapes(faces, args);
+      });
+  }
+
+  sideFaces(...args: (number | FaceFilterBuilder)[]): SceneObject {
+    const suffix = this.buildSuffix('side-faces', args);
+    return new LazySceneObject(`${this.generateUniqueName(suffix)}`,
+      () => {
+        const faces = this.getState('side-faces') as Face[] || [];
+        return this.resolveShapes(faces, args);
+      });
+  }
+
+  startEdges(...args: (number | EdgeFilterBuilder)[]): SceneObject {
+    const suffix = this.buildSuffix('start-edges', args);
+    return new LazySceneObject(`${this.generateUniqueName(suffix)}`,
+      () => {
+        const faces = this.getState('start-faces') as Face[] || [];
+        const edges = faces.flatMap(f => f.getEdges());
+        return this.resolveShapes(edges, args);
+      });
+  }
+
+  endEdges(...args: (number | EdgeFilterBuilder)[]): SceneObject {
+    const suffix = this.buildSuffix('end-edges', args);
+    return new LazySceneObject(`${this.generateUniqueName(suffix)}`,
+      () => {
+        const faces = this.getState('end-faces') as Face[] || [];
+        const edges = faces.flatMap(f => f.getEdges());
+        return this.resolveShapes(edges, args);
+      });
+  }
+
+  sideEdges(...args: (number | EdgeFilterBuilder)[]): SceneObject {
+    const suffix = this.buildSuffix('side-edges', args);
+    return new LazySceneObject(`${this.generateUniqueName(suffix)}`,
+      () => {
+        const sideFaces = this.getState('side-faces') as Face[] || [];
+        const startFaces = this.getState('start-faces') as Face[] || [];
+        const endFaces = this.getState('end-faces') as Face[] || [];
+        const excludedEdges = [...startFaces, ...endFaces].flatMap(f => f.getEdges());
+        const edges = sideFaces.flatMap(f => f.getEdges())
+          .filter(e => !excludedEdges.some(ex => e.getShape().IsSame(ex.getShape())));
+        return this.resolveShapes(edges, args);
+      });
+  }
+
+  private buildSuffix(prefix: string, args: any[]): string {
+    if (args.length === 0) {
+      return prefix;
+    }
+    const key = args.map(a => typeof a === 'number' ? a : 'f').join('-');
+    return `${prefix}-${key}`;
+  }
+
+  private resolveShapes<T extends Shape>(shapes: T[], args: (number | FilterBuilderBase<T>)[]): T[] {
+    if (args.length === 0) {
+      return shapes;
+    }
+
+    if (args.every(a => typeof a === 'number')) {
+      const indices = args as number[];
+      return indices.filter(i => i >= 0 && i < shapes.length).map(i => shapes[i]);
+    }
+
+    const filters = args.filter(a => a instanceof FilterBuilderBase) as FilterBuilderBase<T>[];
+    return new ShapeFilter(shapes as any, ...filters).apply() as T[];
   }
 
   private getWiresFromSceneObject(obj: SceneObject): Wire[] {
@@ -109,12 +234,12 @@ export class Loft extends SceneObject {
       return false;
     }
 
-    if (this.faces.length !== other.faces.length) {
+    if (this.profiles.length !== other.profiles.length) {
       return false;
     }
 
-    for (let i = 0; i < this.faces.length; i++) {
-      if (!this.faces[i].compareTo(other.faces[i])) {
+    for (let i = 0; i < this.profiles.length; i++) {
+      if (!this.profiles[i].compareTo(other.profiles[i])) {
         return false;
       }
     }
@@ -128,7 +253,7 @@ export class Loft extends SceneObject {
 
   serialize() {
     return {
-      faces: this.faces.map(f => f.serialize()),
+      profiles: this.profiles.map(f => f.serialize()),
     }
   }
 }

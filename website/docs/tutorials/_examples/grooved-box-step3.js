ef23f7756836a213eefc6e9cb9292a60d4096809
Marwan Aouida <marwan@aouida.com>
2026-04-15 19:57:01 +0100

    docs: new tutorial for grooved box

---
+++ website/docs/tutorials/_examples/grooved-box-step3.js
@@ -0,0 +1,33 @@
+// @screenshot waitForInput
+import { sketch, extrude, select, fillet, rect, shell, intersect, repeat } from 'fluidcad/core';
+import { face } from 'fluidcad/filters';
+
+sketch("xy", () => {
+    rect(170, 100).radius(18).centered();
+});
+
+const e = extrude(23.6);
+
+const s = shell(-5, e.endFaces())
+
+fillet(8, s.internalEdges())
+
+const facesX = select(face().intersectsWith("front").notOnPlane("xy"))
+
+const s1 = sketch("front", () => {
+    intersect(facesX);
+});
+
+const facesY = select(face().intersectsWith("left").notOnPlane("xy"))
+
+const s2 = sketch("left", () => {
+    intersect(facesY);
+});
+
+const grooveX = extrude(3, s1).thin(-1).remove().symmetric();
+
+repeat("linear", "y", {
+    count: 3,
+    offset: 25,
+    centered: true
+}, grooveX);

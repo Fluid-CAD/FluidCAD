9320b93ebbe4007d5aecf97c5d01c496c22edba2
Marwan Aouida <marwan@aouida.com>
2026-04-22 17:42:31 +0100

    bug fix: fix plane rotation bug + remove rotationSpace

--- lib/math/index.ts
+++ lib/math/index.ts
@@ -3,6 +3,6 @@ export { Point, Point2D, PointLike, Point2DLike, isPointLike, isPoint2DLike } fr
 export { Quaternion } from "./quaternion.js";
 export { Matrix4 } from "./matrix4.js";
 export { Axis, AxisLike, StandardAxis, AxisTransformOptions, isAxisLike, isStandardAxis, toAxis } from "./axis.js";
-export { Plane, PlaneLike, StandardPlane, PlaneTransformOptions, RotationSpace, isPlaneLike, toPlane } from "./plane.js";
+export { Plane, PlaneLike, StandardPlane, PlaneTransformOptions, isPlaneLike, toPlane } from "./plane.js";
 export { CoordinateSystem } from "./coordinate-system.js";
 export { Convert } from "./convert.js";

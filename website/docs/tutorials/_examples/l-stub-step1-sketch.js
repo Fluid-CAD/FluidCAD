059d14da348818a5ec35ccfeb59595428e626c15
Marwan Aouida <marwan@aouida.com>
2026-04-28 10:43:37 +0100

    docs: add new l-stub tutorial

---
+++ website/docs/tutorials/_examples/l-stub-step1-sketch.js
@@ -0,0 +1,10 @@
+// @screenshot waitForInput
+import { enclosing } from "fluidcad/constraints";
+import { aLine, back, circle, cut, extrude, fillet, fuse, hLine, hMove, intersect, line, mirror, move, rect, sketch, slot, subtract, tLine, vLine, vMove } from "fluidcad/core";
+
+sketch("top", () => {
+    const s = slot(82, 26);
+    const f = fuse(circle(24).name("Hole"),
+        rect(-30, 8).centered('vertical').name("Slot Cut"));
+    subtract(s, f)
+}).name("Base Sketch");
---
+++ website/docs/tutorials/_examples/l-stub-step1.js
@@ -0,0 +1,12 @@
+// @screenshot waitForInput
+import { enclosing } from "fluidcad/constraints";
+import { aLine, back, circle, cut, extrude, fillet, fuse, hLine, hMove, intersect, line, mirror, move, rect, sketch, slot, subtract, tLine, vLine, vMove } from "fluidcad/core";
+
+sketch("top", () => {
+    const s = slot(82, 26);
+    const f = fuse(circle(24).name("Hole"),
+        rect(-30, 8).centered('vertical').name("Slot Cut"));
+    subtract(s, f)
+}).name("Base Sketch");
+
+const base = extrude(70).name("Base")

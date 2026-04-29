69074c53cc6b9940ebf2971b6d9e6d849b2c6a61
Marwan Aouida <marwan@aouida.com>
2026-04-29 19:14:07 +0100

    docs: new drafted box tutorial

---
+++ website/docs/tutorials/_examples/drafted-box-step2.js
@@ -0,0 +1,14 @@
+// @screenshot waitForInput
+import { aLine, back, circle, cut, extrude, fillet, hLine, line, local, mirror, plane, rect, remove, repeat, select, shell, sketch, vMove } from "fluidcad/core";
+import { edge, face } from "fluidcad/filters";
+
+sketch(plane("top", 1.50), () => {
+    rect(7, 5).centered()
+});
+
+const base = extrude(-1.5).draft(-8);
+
+fillet(.750, base.sideEdges())
+fillet(.50, select(edge().onPlane("top")))
+
+shell(-.250, select(face().onPlane("top", 1.5)))
--- website/sidebars.ts
+++ website/sidebars.ts
@@ -27,7 +27,7 @@ const sidebars: SidebarsConfig = {
         type: 'doc',
         id: 'tutorials/index',
       },
-      items: ['tutorials/lantern', 'tutorials/ice-cube-tray', 'tutorials/upper-alignment-clamp', 'tutorials/gear-housing', 'tutorials/cswp-sample-exam', 'tutorials/grooved-box', 'tutorials/flange-with-notch', 'tutorials/hinge-bracket', 'tutorials/l-stub'],
+      items: ['tutorials/lantern', 'tutorials/ice-cube-tray', 'tutorials/upper-alignment-clamp', 'tutorials/gear-housing', 'tutorials/cswp-sample-exam', 'tutorials/grooved-box', 'tutorials/flange-with-notch', 'tutorials/hinge-bracket', 'tutorials/l-stub', 'tutorials/drafted-box'],
     },
   ],
   apiSidebar: [

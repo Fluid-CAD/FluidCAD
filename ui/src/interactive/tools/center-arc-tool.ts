192022fee57e3c66a8475e1f232b4310211c8ae3
Marwan Aouida <marwan@aouida.com>
2026-05-11 19:51:22 +0100

    feat: add numericOnly mode to expression input for arc tools

--- ui/src/interactive/tools/center-arc-tool.ts
+++ ui/src/interactive/tools/center-arc-tool.ts
@@ -222,6 +222,7 @@ export class CenterArcTool extends SketchTool {
         clientX: this.lastClientX,
         clientY: this.lastClientY,
         variables: this.cachedVariables,
+        numericOnly: true,
         onCommit: (result) => this.commitFromExpression(result),
       });
     } else {

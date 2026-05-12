9d79f8e6b56aade6d6e6266fc96f322c8a690ff3
Marwan Aouida <marwan@aouida.com>
2026-05-12 10:11:44 +0100

    feat: add center meta-shape vertices to slot, ellipse, and tangent arc features

--- lib/features/2d/tarc.ts
+++ lib/features/2d/tarc.ts
@@ -60,6 +60,9 @@ export class TangentArc extends GeometrySceneObject {
     this.setTangent(new Point2D(tx, ty));
 
     this.addShape(edge);
+    const centerVertex = Vertex.fromPoint2D(centerPoint);
+    centerVertex.markAsMetaShape();
+    this.addShape(centerVertex);
     this.setCurrentPosition(endPoint);
   }
 

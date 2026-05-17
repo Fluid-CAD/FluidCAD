63b3781eb7918b47f81dffed4762c2e17a7537f2
Marwan Aouida <marwan@aouida.com>
2026-05-17 16:15:11 +0100

    feat(tarc): add tArc(radius, target) overload ending at first intersection

---
+++ lib/features/2d/tarc-radius-to-object.ts
@@ -0,0 +1,67 @@
+import { GeometrySceneObject } from "./geometry.js";
+import { Vertex } from "../../common/vertex.js";
+import { QualifiedSceneObject } from "./constraints/qualified-geometry.js";
+import { solveTangentArcRadiusToObject } from "../../oc/constraints/geometric/tangent-arc-radius-to-object.js";
+
+export class TangentArcRadiusToObject extends GeometrySceneObject {
+  constructor(public radius: number, public target: QualifiedSceneObject) {
+    super();
+  }
+
+  build() {
+    const plane = this.sketch.getPlane();
+    const startPoint = this.getCurrentPosition();
+    const startTangent = this.sketch.getTangentAt(this);
+    const targetShape = this.target.toQualifiedShape();
+
+    const result = solveTangentArcRadiusToObject(
+      plane,
+      startPoint,
+      startTangent,
+      this.radius,
+      targetShape
+    );
+
+    for (let i = 0; i < result.edges.length; i++) {
+      this.setState(`edge-${i}`, result.edges[i]);
+    }
+
+    if (result.edges.length > 0) {
+      const lastEdge = result.edges[result.edges.length - 1];
+      const localStart = plane.worldToLocal(lastEdge.getFirstVertex().toPoint());
+      const localEnd = plane.worldToLocal(lastEdge.getLastVertex().toPoint());
+
+      this.setState('start', Vertex.fromPoint2D(localStart));
+      this.setState('end', Vertex.fromPoint2D(localEnd));
+
+      if (result.endTangent) {
+        this.setTangent(result.endTangent);
+      }
+      this.setCurrentPosition(localEnd);
+    }
+
+    this.addShapes(result.edges);
+  }
+
+  compareTo(other: TangentArcRadiusToObject): boolean {
+    if (!(other instanceof TangentArcRadiusToObject)) {
+      return false;
+    }
+    if (!super.compareTo(other)) {
+      return false;
+    }
+    return this.radius === other.radius && this.target.compareTo(other.target);
+  }
+
+  getType(): string {
+    return 'tarc';
+  }
+
+  getUniqueType(): string {
+    return 'tarc-radius-to-object';
+  }
+
+  serialize() {
+    return { radius: this.radius };
+  }
+}

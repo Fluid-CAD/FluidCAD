00b3c99dbf7f5754774980dda56eff30b2c42d49
Marwan Aouida <marwan@aouida.com>
2026-05-18 06:48:03 +0100

    feat: add init options with mesh deflection configuration

--- lib/rendering/render-face.ts
+++ lib/rendering/render-face.ts
@@ -1,7 +1,8 @@
 import { Shape } from "../common/shape.js";
 import { Mesh } from "../oc/mesh.js";
+import type { MeshConfig } from "../oc/mesh.js";
 
-export function renderFace(faceObj: Shape, vertexOffset: number = 0) {
+export function renderFace(faceObj: Shape, vertexOffset: number = 0, meshConfig?: MeshConfig) {
   const face = faceObj.getShape();
-  return Mesh.triangulateFaceRaw(face, vertexOffset);
+  return Mesh.triangulateFaceRaw(face, vertexOffset, meshConfig);
 }

1a76c660237759f06aa820291976aebce9c29e0b
Marwan Aouida <marwan@aouida.com>
2026-08-12 12:04:57 +0100

    docs(tutorials): filter gear housing chamfer to yz-plane end edges via edge().onPlane('yz', 31.5)

--- website/docs/tutorials/_examples/gear-housing-step1.js
+++ website/docs/tutorials/_examples/gear-housing-step1.js
@@ -1,4 +1,5 @@
 import { arc, chamfer, circle, copy, cut, extrude, fillet, hMove, mirror, move, plane, pMove, rect, repeat, sketch, vMove } from "fluidcad/core";
+import { edge } from "fluidcad/filters";
 
 let supportWidth = (150 - 63) / 2;
 let supportThickness = 12;
@@ -12,4 +13,4 @@ const e1 = extrude(supportThickness);
 
 fillet(12, e1.sideEdges(2, 3));
 
-chamfer(8, 90 - 25, true, e1.endEdges())
+chamfer(8, 90 - 25, true, e1.endEdges(edge().onPlane('yz', 31.5)))

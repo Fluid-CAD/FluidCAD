77292941a29842dc4f2497a0315ccbf884457b51
Marwan Aouida <marwan@aouida.com>
2026-09-23 15:50:14 +0100

    docs(website): region picking leads with the viewport flow and reads the keys a pick writes

--- website/docs/3d-operations/_examples/extrude-region-pick.js
+++ website/docs/3d-operations/_examples/extrude-region-pick.js
@@ -2,8 +2,8 @@ import { sketch, extrude } from 'fluidcad/core';
 import { circle } from 'fluidcad/core';
 
 sketch("xy", () => {
-    circle([0, 0], 60);
-    circle([0, 0], 30);
-  })
+    const outer = circle([0, 0], 60);
+    const inner = circle([0, 0], 30);
+})
 
-extrude(20).pick([20, 0])
+extrude(20).region('outer')   // written by the pick: the ring, whose outer loop is `outer`
